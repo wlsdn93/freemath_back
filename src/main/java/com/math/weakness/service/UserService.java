c7956401d1d85c4326607807a8a22fbaddc5b439
wlsdn93 <84765181+wlsdn93@users.noreply.github.com>
2021-12-29 23:37:39 +0900

    get user id from httpsession

--- src/main/java/com/math/weakness/service/UserService.java
+++ src/main/java/com/math/weakness/service/UserService.java
@@ -1,10 +1,18 @@
 package com.math.weakness.service;
 
+import com.math.weakness.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    /**
-     * 정답 제출
-     */
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Long findByEmail(String email) {
+       return userRepository.findByEmail(email).get().getUserId();
+    }
 }
--- src/main/java/com/math/weakness/web/ProblemController.java
+++ src/main/java/com/math/weakness/web/ProblemController.java
@@ -5,7 +5,7 @@ import com.math.weakness.domain.ProblemShow;
 import com.math.weakness.dto.ProblemRequestDto;
 import com.math.weakness.dto.ProblemResponseDto;
 import com.math.weakness.service.ProblemService;
-import com.querydsl.core.Tuple;
+import com.math.weakness.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
@@ -16,6 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
 
@@ -24,13 +25,17 @@ import java.io.IOException;
 public class ProblemController {
 
     private final ProblemService problemService;
+    private final UserService userService;
+    private final HttpSession httpSession;
 
     @Value("${file.dir}")
     private String fileDir;
 
     @Autowired
-    public ProblemController(ProblemService problemService) {
+    public ProblemController(ProblemService problemService, UserService userService, HttpSession httpSession) {
         this.problemService = problemService;
+        this.userService = userService;
+        this.httpSession = httpSession;
     }
 
     @GetMapping
@@ -38,8 +43,9 @@ public class ProblemController {
                            @RequestParam(required = false) Integer difficulty,
                            @PageableDefault Pageable pageable,
                            Model model) {
-
-        Page<ProblemShow> problems = problemService.showAllProblemsByUser(pageable, 1L);
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        Long id = userService.findByEmail(user.getEmail());
+        Page<ProblemShow> problems = problemService.showAllProblemsByUser(pageable, id);
         model.addAttribute("problems", problems);
 
         return "problems";
