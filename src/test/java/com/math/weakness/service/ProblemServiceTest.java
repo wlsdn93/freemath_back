18096bbabc9161c80fbfead1c053481ded36a621
wlsdn93 <84765181+wlsdn93@users.noreply.github.com>
2022-01-14 17:51:23 +0900

    code for making 'UserProblem' dummy data

--- src/test/java/com/math/weakness/service/ProblemServiceTest.java
+++ src/test/java/com/math/weakness/service/ProblemServiceTest.java
@@ -1,7 +1,9 @@
 package com.math.weakness.service;
 
 import com.math.weakness.domain.Problem;
+import com.math.weakness.domain.UserProblem;
 import com.math.weakness.dto.ProblemRequestDto;
+import com.math.weakness.dto.UserProblemDto;
 import com.math.weakness.repository.ProblemRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +13,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -24,10 +28,6 @@ class ProblemServiceTest {
     @Autowired
     ProblemRepository problemRepository;
 
-    @BeforeEach
-    public void BeforeEach() {
-        problemRepository.deleteAll();
-    }
 
     @Test
     @DisplayName("문제추가")
@@ -78,19 +78,46 @@ class ProblemServiceTest {
     @Test
     @Rollback(value = false)
     void testSampleAdd() {
+        int[] difficulties = {2, 3, 4};
         for( int i = 0 ; i < 1000 ; i++) {
             //Given
             ProblemRequestDto problemRequestDto1 = ProblemRequestDto.builder()
                     .title("title1")
                     .answer("answer1")
                     .author("author1")
-                    .difficulty(4)
+                    .difficulty(difficulties[new Random().nextInt(difficulties.length)])
                     .build();
 
             //When
-            Long problemId1 = problemService.addProblem(problemRequestDto1);
+            problemService.addProblem(problemRequestDto1);
 
         }
     }
 
+    @Test
+    @Rollback(value = false)
+    void saveResult() {
+        Long userId = 1001L;
+        String[] answer = {"answer1", "answer2"};
+        for (long problemId = 21L; problemId <= 50 ; problemId++) {
+            problemService.saveResult(UserProblemDto.builder()
+                    .problemId(problemId)
+                    .userId(userId)
+                    .answer(answer[new Random().nextInt(answer.length)])
+                    .build());
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
