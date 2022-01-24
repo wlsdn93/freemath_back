4e2eca0a5b265f4499883649e29c764dfcaf17bc
shirohoo <shirohoo4102@gmail.com>
2022-01-24 20:53:49 +0900

    :recycle: 스프링 시큐리티 설정파일 리팩토링
    프레임워크 제공 기능 최대한 활용하여 가독성 좋게 변경

--- src/main/java/com/math/weakness/config/auth/SecurityConfig.java
+++ src/main/java/com/math/weakness/config/auth/SecurityConfig.java
@@ -2,32 +2,44 @@ package com.math.weakness.config.auth;
 
 import com.math.weakness.domain.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
-@RequiredArgsConstructor
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(
+                        PathRequest.toH2Console(),
+                        PathRequest.toStaticResources().atCommonLocations()
+                );
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                    .headers().frameOptions().disable()
-                    .and()
-                        .authorizeHttpRequests()
-                        .antMatchers("/","/css/**","/js/**","/images/**","/problems").permitAll()
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(header -> header
+                        .frameOptions().disable())
+                .authorizeHttpRequests(requests -> requests
                         .antMatchers("/problems/add").hasRole(Role.ADMIN.name())
                         .anyRequest().authenticated()
-                    .and()
-                        .logout()
+                )
+                .logout(logout -> logout
                         .logoutSuccessUrl("/")
-                    .and()
-                        .oauth2Login()
+                )
+                .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint()
-                        .userService(customOAuth2UserService);
+                        .userService(customOAuth2UserService)
+                );
     }
 }
