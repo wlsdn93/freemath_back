776a1e157c5efd31556406e56a5ae21ace7d12a5
wlsdn93 <84765181+wlsdn93@users.noreply.github.com>
2022-02-04 17:07:39 +0900

    problem detail, save result

--- src/main/java/com/math/weakness/dto/ProblemResponse.java
+++ src/main/java/com/math/weakness/dto/ProblemResponse.java
@@ -2,34 +2,31 @@ package com.math.weakness.dto;
 
 import com.math.weakness.domain.Problem;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-public class ProblemResponseDto {
+public class ProblemResponse {
 
     private final Long problemId;
     private final String title;
     private final String answer;
+    private final String answerType;
     private final String problemImageName;
     private final String solutionImageName;
-    private final Integer difficulty;
-    private final LocalDateTime createdDate;
-    private final LocalDateTime modifiedDate;
+    private final String difficulty;
 
-    private ProblemResponseDto(Problem entity) {
+    private ProblemResponse(Problem entity) {
         this.problemId = entity.getProblemId();
         this.title = entity.getTitle();
         this.answer = entity.getAnswer();
+        this.answerType = entity.getAnswerType();
         this.problemImageName = entity.getProblemImageName();
         this.solutionImageName = entity.getSolutionImageName();
         this.difficulty = entity.getDifficulty();
-        this.createdDate = entity.getCreatedDate();
-        this.modifiedDate = entity.getModifiedDate();
     }
 
-    public static ProblemResponseDto from(Problem entity) {
-        return new ProblemResponseDto(entity);
+    public static ProblemResponse from(Problem entity) {
+        return new ProblemResponse(entity);
     }
 }
