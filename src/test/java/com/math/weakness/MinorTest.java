d5c8857ecb04bb8a75b434567db21e4f096aa1a1
wlsdn93 <84765181+wlsdn93@users.noreply.github.com>
2022-02-04 14:03:55 +0900

    image upload service

---
+++ src/test/java/com/math/weakness/MinorTest.java
@@ -0,0 +1,18 @@
+package com.math.weakness;
+
+
+import org.junit.jupiter.api.Test;
+
+public class MinorTest {
+
+    @Test
+    void test() {
+        String sample = "123,535,132";
+        String[] split = sample.split(",");
+        System.out.println("split1 = " + split[0]);
+        System.out.println("split2 = " + split[1]);
+        System.out.println("split3 = " + split[2]);
+
+    }
+
+}
