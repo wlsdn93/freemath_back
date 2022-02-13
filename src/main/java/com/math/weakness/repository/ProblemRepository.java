2b3dade67f63d22dc2059cfb9acc68f1907ba9b7
wlsdn93 <84765181+wlsdn93@users.noreply.github.com>
2022-02-13 15:35:09 +0900

    refactor and add getProblemIdList logic for random-problem

--- src/main/java/com/math/weakness/repository/ProblemRepository.java
+++ src/main/java/com/math/weakness/repository/ProblemRepository.java
@@ -2,6 +2,7 @@ package com.math.weakness.repository;
 
 import com.math.weakness.domain.Problem;
 import com.math.weakness.dto.ProblemShow;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,8 +11,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProblemRepository extends JpaRepository<Problem, Long>, CustomProblemRepository {
 
     @Override
-    Page<ProblemShow> findByDifficultyAndStatusAndId(Long id, Pageable pageable, Integer difficulty, Boolean status, String subject);
+    Page<ProblemShow> getPageForUser(Long id, Pageable pageable, Integer difficulty, Boolean status, String subject);
 
     @Override
-    Page<ProblemShow> findByDifficultyAndStatus(Pageable pageable, Integer difficulty, Boolean status, String subject);
+    Page<ProblemShow> getPageForGuest(Pageable pageable, Integer difficulty, Boolean status, String subject);
+
+    @Override
+    List<Long> problemIdListForUser(Long id, Integer difficulty, Boolean status, String subject);
+
+    @Override
+    List<Long> problemIdListForGuest(Integer difficulty, Boolean status, String subject);
 }
